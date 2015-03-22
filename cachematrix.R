21088910f05d00135a215f3a53a269c4e1232b1c
Charles <cmcfeaters@gmail.com>
2015-03-22 16:22:48 -0400

    minor COrrections

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,19 @@
 ## Put comments here that give an overall description of what your
-## functions do
+#makeCacheMatrix takes in a matrix creates an cachable matrix object
+#the item returned has 4 functins as described below
+
+#the cachesolve takes in a matrix object, checks if it has a cached inverse and either
+#returns taht value or calcuates, caches and returns the inverse
 
 ## Write a short comment describing this function
+#this takes a matrix and returns an list with 4 functions
+	#we return a list containng:
+	#1 a set function
+	#2 a get function
+	#3 a get inverse function
+	#4 a set inverser function
 
 makeCacheMatrix <- function(x = matrix()) {
-	#there is an issue solving a 3x3 matrix.  it says it must be numeric.  need to figure oout
-
 	#inverse flag is set to NULL upon creation, we haven't calculated it yet
 	inv<-NULL	
 
@@ -24,35 +32,32 @@ makeCacheMatrix <- function(x = matrix()) {
 		inv<<-solve
 	}	
 
-	#we return a list containng:
-	#1 a set function
-	#2 a get function
-	#3 a get inverse function
-	#4 a set inverser function
+
 
 	list(set=set, get=get, getInverse=getInverse, setInverse=setInverse)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
       ## Return a matrix that is the inverse of 'x'
 	#cache solve is designed to return an inverted matrix to the user
 	#the function uses the cached matrix object to return either
 	#the cached inverse or the newly calculated inverse
+
+cacheSolve <- function(x, ...) {
+
 	tInv<-x$getInverse()
 	
 	if(!is.null(tInv)){	#if the inverse of x exists in cache (isn't null)
 		#the cache exists, return it
-		message("In THE MUTHAFUCKIN CACHE")
+		message("Cached")
 		return(x$getInverse())
 	}else{
 	#else, the cache does not exist and we need to get it
 		tMat<-x$get()		#get the matrix
 		nInv<-FALSE
 		tryCatch({
-			iMat<<-solve(x)
+			iMat<<-solve(x$get())
 			},error=function(e){
 			nInv<<-TRUE
 			})
